7e03b585923cc9638e565dcd06f5a3b51aa2b6d7
ch0fas <sof.mal.gar55@gmail.com>
2026-01-06 18:48:50 -0600

    Created quick-c script

---
+++ quick_c.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Quick-C,  Simple script for compiling a single C file and running it on the terminal, deleting previously compiled binary if it already exists, by me
+
+# Function to delete the previous binary
+delete_binary()
+{
+    BINARY_NAME="${1::-2}"
+    rm $BINARY_NAME
+}
+
+# Main Flow -> Checking if a previous binary exists
+if [ -f "${1::-2}" ];
+then
+    echo "Old binary found, creating new one"
+    delete_binary "$1"
+    BINARY_NAME="${1::-2}"
+    gcc $1 -o $BINARY_NAME
+    ./$BINARY_NAME ${@:2}
+else
+    echo "Creating binary for first time"
+    BINARY_NAME="${1::-2}"
+    gcc $1 -o $BINARY_NAME
+    ./$BINARY_NAME ${@:2}
+fi
